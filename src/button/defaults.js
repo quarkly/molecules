58f7788bcc299f92d65cd0d675f2f66fa706a266
Aleksandr Kudinov <aesarine@gmail.com>
2020-07-17 19:53:51 +0300

    feat: Style defaults for components

---
+++ src/button/defaults.js
@@ -0,0 +1,26 @@
+export default {
+	'type': 'submit',
+	'appearance': 'button',
+
+	'padding-top': '8px',
+	'padding-bottom': '8px',
+	'padding-left': '24px',
+	'padding-right': '24px',
+
+	'font': '--font-base',
+
+	'color': '--color-light',
+	'background': '--color-primary',
+	'disabled-background': '--color-grey',
+	'focus-box-shadow': '0 0 0 2px --color-primary',
+
+	'border-width': '0px',
+	'border-style': 'none',
+	'border-radius': '2px',
+	'outline': 'none',
+
+	'user-select': 'none',
+	'box-sizing': 'border-box',
+	'overflow': 'visible',
+	'cursor': 'pointer',
+};
--- src/button/index.js
+++ src/button/index.js
@@ -1,55 +1,33 @@
-import React from 'react';
-import atomize from "@quarkly/atomize";
+import atomize from '@quarkly/atomize';
+
+import defaults from './defaults';
 
 export default atomize.button(
 	{
-		name: "Button",
+		name: 'Button',
 		effects: {
-			hover: ":hover",
-			focus: ":focus",
-			active: ":active",
-			disabled: ":disabled"
+			hover: ':hover',
+			focus: ':focus',
+			active: ':active',
+			disabled: ':disabled',
 		},
 		description: {
-			en: "button element — button that performs an action. For example, can be used in forms to submit data",
-			ru: "Элемент button — кнопка, которая выполняет какое-либо действие. Например, отправка данных формы"
+			en:
+				'button element — button that performs an action. For example, can be used in forms to submit data',
+			ru:
+				'Элемент button — кнопка, которая выполняет какое-либо действие. Например, отправка данных формы',
 		},
 		propInfo: {
 			type: {
-				control: "select",
-				variants: ["submit", "button"],
-				category: "Main"
+				control: 'select',
+				variants: ['submit', 'button'],
+				category: 'Main',
 			},
 			disabled: {
-				control: "checkbox",
-				category: "Main"
-			}
-		}
+				control: 'checkbox',
+				category: 'Main',
+			},
+		},
 	},
-	{
-		"type": "submit",
-		"appearance": "button",
-		
-		"padding-top": "8px",
-		"padding-bottom": "8px",
-		"padding-left": "24px",
-		"padding-right": "24px",
-		
-		"font": "--font-base",
-		
-		"color": "--color-light",
-		"background": "--color-primary",
-		"disabled-background": "--color-grey",
-		"focus-box-shadow": "0 0 0 2px --color-primary",
-		
-		"border-width": "0px",
-		"border-style": "none",
-		"border-radius": "2px",
-		"outline": "none",
-		
-		"user-select": "none",
-		"box-sizing": "border-box",
-		"overflow": "visible",
-		"cursor": "pointer"
-	}
+	defaults,
 );
