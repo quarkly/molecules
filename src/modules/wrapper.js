42081466719fafa4f9e0618730f71a063bad9618
Aleksandr Kudinov <aesarine@gmail.com>
2020-07-29 16:26:45 +0300

    fix: New Wrapper because of negative margins

--- src/modules/wrapper.js
+++ src/modules/wrapper.js
@@ -1,20 +1,33 @@
+import React from 'react';
 import atomize from '@quarkly/atomize';
 
-export default atomize.div(
+const RootParent = atomize.div();
+const RootChild = atomize.div();
+
+const Wrapper = ({ children }) =>
+	<RootParent
+		margin="16px 8px"
+		display="flex"
+	>
+		<RootChild
+			width="100%"
+
+			color="#000000"
+			background-color="#FFFFFF"
+			box-shadow="0 0 48px 0 rgba(0,0,0,.2)"
+
+			font-family="sans-serif"
+
+			user-select="none"
+			position="relative"
+			display="block"
+		>
+			{ children }
+		</RootChild>
+	</RootParent>
+
+export default atomize(Wrapper)(
 	{
-		name: 'Wrapper',
-		propInfo: {},
-	},
-	{
-		'margin': '16px 8px',
-
-		'color': '#000000',
-		'background-color': '#FFFFFF',
-		'box-shadow': '0 0 48px 0 rgba(0,0,0,.2)',
-
-		'font-family': 'sans-serif',
-
-		'user-select': 'none',
-		'position': 'relative',
-	},
+		name: 'Wrapper'
+	}
 );
