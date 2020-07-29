474ebb9576f30a5a52fa4af88a1ea9be96eaab0d
Aleksandr Kudinov <aesarine@gmail.com>
2020-07-29 16:24:42 +0300

    feat: Added stories for Stack component

--- src/components/stack/section.stories.js
+++ src/components/stack/section.stories.js
@@ -14,15 +14,81 @@ const stories = storiesOf('Components/Stack', module);
 
 stories.addDecorator(withKnobs);
 
-stories.add('default', () => (
+stories.add('2 items (default)', () => (
 	<Theme>
 		<Wrapper>
 			<Stack {...getKnobs(defaults)}>
 				<StackItem width="50%">
-					<Text margin="0">First Item</Text>
+					<Text>First Item</Text>
 				</StackItem>
 				<StackItem width="50%">
-					<Text margin="0">Second Item</Text>
+					<Text>Second Item</Text>
+				</StackItem>
+			</Stack>
+		</Wrapper>
+	</Theme>
+));
+
+stories.add('3 items', () => (
+	<Theme>
+		<Wrapper>
+			<Stack {...getKnobs(defaults)}>
+				<StackItem width="33.33%">
+					<Text>First Item</Text>
+				</StackItem>
+				<StackItem width="33.33%">
+					<Text>Second Item</Text>
+				</StackItem>
+				<StackItem width="33.33%">
+					<Text>Third Item</Text>
+				</StackItem>
+			</Stack>
+		</Wrapper>
+	</Theme>
+));
+
+stories.add('4 items', () => (
+	<Theme>
+		<Wrapper>
+			<Stack {...getKnobs(defaults)}>
+				<StackItem width="25%">
+					<Text>First Item</Text>
+				</StackItem>
+				<StackItem width="25%">
+					<Text>Second Item</Text>
+				</StackItem>
+				<StackItem width="25%">
+					<Text>Third Item</Text>
+				</StackItem>
+				<StackItem width="25%">
+					<Text>Fourth Item</Text>
+				</StackItem>
+			</Stack>
+		</Wrapper>
+	</Theme>
+));
+
+stories.add('6 items', () => (
+	<Theme>
+		<Wrapper>
+			<Stack {...getKnobs(defaults)}>
+				<StackItem width="16.66%">
+					<Text>First Item</Text>
+				</StackItem>
+				<StackItem width="16.66%">
+					<Text>Second Item</Text>
+				</StackItem>
+				<StackItem width="16.66%">
+					<Text>Third Item</Text>
+				</StackItem>
+				<StackItem width="16.66%">
+					<Text>Fourth Item</Text>
+				</StackItem>
+				<StackItem width="16.66%">
+					<Text>Fifth Item</Text>
+				</StackItem>
+				<StackItem width="16.66%">
+					<Text>Sixth Item</Text>
 				</StackItem>
 			</Stack>
 		</Wrapper>
