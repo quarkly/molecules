0cc99e83ec45e74aa2f71d79510f509a629f4ff4
Aleksandr Kudinov <aesarine@gmail.com>
2020-08-12 12:43:33 +0300

    feat: Video component

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ export { default as Formspree } from './components/formspree';
 export { default as GoogleMap } from './components/googlemap';
 export { default as Menu } from './components/menu';
 export { default as SocialMedia } from './components/socialMedia';
+export { default as Video } from './components/video';
 
 // Inline wysiwyg components
 export { default as Inline } from './wysiwyg/inline';
